4b90907b7c46a9b2a7e1fbc9d15eb753ca608d2a
Liam Griffin <liam.griffin@datummonitoring.com>
2016-02-15 18:58:42 +0000

    Added Post class and tests #1

---
+++ SocialNetwork.Core.Tests/Post_Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocialNetwork.Core.Tests.Helpers;
+
+namespace SocialNetwork.Core.Tests
+{
+    [TestClass]
+    public class Post_Tests
+    {
+        [TestMethod]
+        public void Instantiated_Post_contains_content_and_time_of_post()
+        {
+            string testMessage = "Good morning everyone!";
+            DateTime mockPostedDateTime = DateTime.UtcNow;
+
+            PostWithFixedDateTime mockPost = new PostWithFixedDateTime(testMessage, mockPostedDateTime);
+
+            Assert.AreEqual(testMessage, mockPost.Content);
+            Assert.AreEqual(mockPostedDateTime, mockPost.PostedDateTime);
+        }
+    }
+}
