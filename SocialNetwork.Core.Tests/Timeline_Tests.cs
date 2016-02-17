7f9e416db9999f1c479c56a8b55b11394c7d959e
Liam Griffin <liam.griffin@datummonitoring.com>
2016-02-17 19:05:24 +0000

    Add ability for User to see their Wall #7

--- SocialNetwork.Core.Tests/Timeline_Tests.cs
+++ SocialNetwork.Core.Tests/Timeline_Tests.cs
@@ -9,32 +9,6 @@ namespace SocialNetwork.Core.Tests
     public class Timeline_Tests
     {
         [TestMethod]
-        public void FormatPost_returns_output_friendly_Post()
-        {
-            string testMessage = "Everything is awesome";
-
-            Post testPost = new Post(testMessage);
-
-            string formattedPost = new Timeline().FormatPost(testPost);
-
-            Assert.AreEqual($"{testMessage} (0 minutes ago)", formattedPost);
-        }
-
-        [TestMethod]
-        public void FormatPost_returns_correct_grammar_for_post_one_minute_ago()
-        {
-            string testMessage = "Everything may or may not be awesome";
-
-            PostWithFixedDateTime postOneMinuteAgo = new PostWithFixedDateTime(
-                testMessage, 
-                DateTime.UtcNow.AddMinutes(-1));
-
-            string formattedPost = new Timeline().FormatPost(postOneMinuteAgo);
-
-            Assert.AreEqual($"{testMessage} (1 minute ago)", formattedPost);
-        }
-
-        [TestMethod]
         public void GetTimeline_returns_formatted_collection_of_posts()
         {
             string testMessage  = "Unable to ascertain the awesomeness of everything";
@@ -42,8 +16,8 @@ namespace SocialNetwork.Core.Tests
 
             Timeline testTimeline = new Timeline();
 
-            testTimeline.AddPost(new Post(testMessage));
-            testTimeline.AddPost(
+            testTimeline.Posts.Add(new Post(testMessage));
+            testTimeline.Posts.Add(
                 new PostWithFixedDateTime(testMessage2, DateTime.UtcNow.AddMinutes(-10)));
 
             IEnumerable<string> formattedPosts = testTimeline.GetTimeline();
