7e7c1a51008a2ede657840aaa8a605247a629e04
Liam Griffin <liam.griffin@datummonitoring.com>
2016-02-20 21:03:15 +0000

    Program.cs methods moved to Commands class #9

---
+++ SocialNetwork.Core.Tests/Commands_Tests.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SocialNetwork.Core.Tests
+{
+    [TestClass]
+    public class Commands_Tests
+    {
+        [TestMethod]
+        public void PostMessage_adds_Post_to_Users_Timeline_when_User_already_exists()
+        {
+            string testUserInput = "Alice -> hello London";
+
+            List<User> testUsers = new List<User>
+            {
+                new User("Alice")
+            };
+
+            Assert.AreEqual(0, testUsers[0].Timeline.Posts.Count);
+
+            Commands.PostMessage(testUserInput, testUsers);
+
+            Assert.AreEqual(1, testUsers[0].Timeline.Posts.Count);
+
+            Assert.AreEqual("hello London", testUsers[0].Timeline.Posts[0].Content);
+        }
+
+        [TestMethod]
+        public void PostMessage_creates_User_if_it_doesnt_exist_and_adds_Post()
+        {
+            string testUserInput = "Alice -> goodnight London";
+
+            List<User> testUsers = new List<User>();
+
+            Assert.AreEqual(0, testUsers.Count);
+
+            Commands.PostMessage(testUserInput, testUsers);
+
+            Assert.AreEqual(1, testUsers.Count);
+
+            Assert.AreEqual(1, testUsers[0].Timeline.Posts.Count);
+
+            Assert.AreEqual("goodnight London", testUsers[0].Timeline.Posts[0].Content);
+        }
+
+        [TestMethod]
+        public void GetFormattedTimeline_returns_expected_timeline_format()
+        {
+            List<User> testUsers = new List<User>();
+
+            User bob = new User("Bob");
+
+            bob.Timeline.Posts.AddRange(new []
+            {
+                new Post
+                {
+                    Content = "good night last night",
+                    PostedDateTime = DateTime.UtcNow.AddMinutes(-10)
+                },
+                new Post("not feeling so great today"), 
+            });
+
+            testUsers.Add(bob);
+
+            string testUserInput = "Bob";
+
+            string result = Commands.GetFormattedTimeline(testUserInput, testUsers);
+
+            Assert.AreEqual(
+                $"not feeling so great today (0 minutes ago){Environment.NewLine}" +
+                    "good night last night (10 minutes ago)", 
+                result);
+        }
+
+        [TestMethod]
+        public void AddUserToFollowingForUser_adds_User_to_Following_for_user()
+        {
+            List<User> testUsers = new List<User>
+            {
+                new User("Bob"),
+                new User("Alice")
+            };
+
+            string testUserInput = "Bob follows Alice";
+
+            Assert.AreEqual(0, testUsers[0].Following.Count);
+
+            Commands.AddUserToFollowingForUser(testUserInput, testUsers);
+
+            Assert.AreEqual(1, testUsers[0].Following.Count);
+
+            Assert.AreEqual("Alice", testUsers[0].Following[0].Name);
+        }
+
+        [TestMethod]
+        public async Task GetWall_returns_expected_wall_format()
+        {
+            List<User> testUsers = new List<User>();
+
+            User bob = new User("Bob");
+
+            bob.Timeline.Posts.Add(new Post
+            {
+                Content = "how is everyone?",
+                PostedDateTime = DateTime.UtcNow.AddMinutes(-10)
+            });
+
+            bob.Timeline.Posts.Add(new Post("I'm feeling rough"));
+
+            User alice = new User("Alice");
+
+            alice.Timeline.Posts.Add(new Post
+            {
+                Content = "up bright and early today",
+                PostedDateTime = DateTime.UtcNow.AddMinutes(-5)
+            });
+
+            await Task.Delay(100);
+
+            alice.Timeline.Posts.Add(new Post("need coffee"));
+
+            bob.Following.Add(alice);
+
+            testUsers.AddRange(new [] { bob, alice });
+
+            string testUserIput = "Bob wall";
+
+            string wall = Commands.GetWall(testUserIput, testUsers);
+
+            Assert.AreEqual(
+                $"Alice - need coffee (0 minutes ago){Environment.NewLine}" +
+                $"Bob - I'm feeling rough (0 minutes ago){Environment.NewLine}" +
+                $"Alice - up bright and early today (5 minutes ago){Environment.NewLine}" +
+                "Bob - how is everyone? (10 minutes ago)",
+                wall);
+        }
+    }
+}
--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 using SocialNetwork.Core;
 
 namespace SocialNetwork
@@ -20,91 +18,18 @@ namespace SocialNetwork
                 string userInput = Console.ReadLine();
 
                 if (userInput.Contains("->"))
-                    PostMessage(userInput);
+                    Commands.PostMessage(userInput, Users);
 
                 else if (userInput.Contains("follows"))
-                    AddUserToFollowingForUser(userInput);
+                    Commands.AddUserToFollowingForUser(userInput, Users);
 
                 else if (userInput.Contains("wall"))
-                    DisplayWall(userInput);
+                    Console.WriteLine(Commands.GetWall(userInput, Users));
 
                 else if (!userInput.Contains(" "))
-                    DisplayTimeline(userInput);
+                    Console.WriteLine(Commands.GetFormattedTimeline(userInput, Users));
             }
 
-        }
-        
-        private static void PostMessage(string userInput)
-        {
-            string[] parsedInput = Regex.Split(userInput, @" -> ");
-
-            User user = Users.FirstOrDefault(u => u.Name == parsedInput[0]);
-
-            if (user != null)
-            {
-                user.Timeline.Posts.Add(new Post(parsedInput[1]));
-            }
-            else
-            {
-                User newUser = new User(parsedInput[0]);
-
-                Users.Add(newUser);
-
-                newUser.Timeline.Posts.Add(new Post(parsedInput[1]));
-            }
-        }
-
-        private static void DisplayTimeline(string userInput)
-        {
-            User user = Users.FirstOrDefault(u => u.Name == userInput);
-
-            if (user == null) return;
-
-            IEnumerable<string> userTimeline = user.Timeline.GetTimeline().Reverse();
-
-            Console.WriteLine(string.Join(Environment.NewLine, userTimeline));
-        }
-
-
-        private static void AddUserToFollowingForUser(string userInput)
-        {
-            string[] parsedInput = Regex.Split(userInput, " follows ");
-
-            User user = Users.FirstOrDefault(u => u.Name == parsedInput[0]);
-
-            User userToFollow = Users.FirstOrDefault(u => u.Name == parsedInput[1]);
-
-            if (userToFollow != null)
-            {
-                user?.Following.Add(userToFollow);
-            }
-                
-        }
-
-        private static void DisplayWall(string userInput)
-        {
-            string[] parsedInput = Regex.Split(userInput, " wall");
-
-            User user = Users.FirstOrDefault(u => u.Name == parsedInput[0]);
-
-            if (user == null) return;
-
-            List<Tuple<string, Post>> wall = new List<Tuple<string, Post>>();
-
-            wall.AddRange(user.Timeline.Posts.Select(
-                post => new Tuple<string, Post>(user.Name, post)));
-
-            foreach (User u in user.Following)
-            {
-                wall.AddRange(
-                    u.Timeline.Posts.Select(
-                        post => new Tuple<string, Post>(u.Name, post)));
-            }
-            
-            foreach (Tuple<string, Post> p in wall.OrderByDescending(t => t.Item2.PostedDateTime))
-            {
-                Console.WriteLine($"{p.Item1} - {p.Item2.ToOutputFormat()}");
-            }
         }
     }
 }
